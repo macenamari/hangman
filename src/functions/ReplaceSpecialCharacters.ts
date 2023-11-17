2ae49b6498ec69d539c7f251a4a024722376620e
Mariana Lucas da Macena <marianadamacena11@gmail.com>
2023-11-17 15:20:07 -0300

    Permitindo caracteres diacríticos

--- src/functions/ReplaceSpecialCharacters.ts
+++ src/functions/ReplaceSpecialCharacters.ts
@@ -1,28 +1,5 @@
 export default function ReplaceSpecialCharacters(word: string) {
-  const a = /[àáâãÁÀÂÃ]/i;
-  const e = /[éêÉÊ]/i;
-  const i = /[íÍ]/i;
-  const o = /[óôõÓÕÔ]/i;
-  const u = /[úüÚÜ]/i;
-  const c = /[çÇ]/;
-
-  if(a.test(word.toLowerCase())){
-    word = word.replace(a, "a");
-  }
-  if(e.test(word.toLowerCase())){
-    word = word.replace(e, "e");
-  }
-  if(i.test(word.toLowerCase())){
-    word = word.replace(i, "i");
-  }
-  if(o.test(word.toLowerCase())){
-    word = word.replace(o, "o");
-  }
-  if(u.test(word.toLowerCase())){
-    word = word.replace(u, "u");
-  }
-  if(c.test(word.toLowerCase())){
-    word = word.replace(c, "c");
-  }
-  return word;
+  return word
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
 };
